069afb3ac8b48592975788f82aeca5f5f038f15b
Alexandru <T_alexandru87@yahoo.com>
2022-04-25 19:36:46 +0100

    timer is rendered and working

--- Assets/assets/js/index.js
+++ Assets/assets/js/index.js
@@ -1,8 +1,13 @@
 // global declarations
 const questions = [];
 let questionIndex = 0;
-let timerValue = 10 * questions.length;
+let timerValue = 10 * 6;
 let quizComplete = false;
+
+// target the html elements
+const startButton = document.getElementById("start-btn");
+const main = document.getElementById("main");
+const contentSection = document.getElementById("main-content");
 
 const onLoad = () => {
   // initialise local storage
@@ -13,15 +18,25 @@ const onLoad = () => {
 const removeStartSection = () => {};
 
 const startTimer = () => {
-  // declare function to execute every 1 sec
-  const countdown = () => {
-    // decrement timer value
-    // if quizComplete is true then stop timer
-    // check if timer reaches 0
-    // if true render game over
-  };
+  console.log("start timer");
 
-  // setInterval of 1000ms (1s)
+  // declare function to execute every 1 sec
+  const timeInterval = setInterval(function () {
+    // decrement timer value
+    timerValue -= 1;
+
+    const timeValue = document.getElementById("time-span");
+    timeValue.textContent = timerValue;
+
+    console.log(timerValue);
+    // if quizComplete is true then stop timer
+
+    // check if timer reaches 0
+    if (timerValue === 0) {
+      clearInterval(timeInterval);
+    }
+    // if true render game over
+  }, 1000); // setInterval of 1000ms (1s)
 };
 
 const validateAnswer = () => {
@@ -49,11 +64,48 @@ const handleFormSubmit = () => {
 };
 
 const renderTimerSection = () => {
+  console.log("render time section");
   // use HTML as guide and build in JS
-  // append section to main
+
+  // create section
+  const section = document.createElement("section");
+  // add class attribute
+  section.setAttribute("class", "timer-section");
+  main.append(section);
+
+  // create h2
+  const h2 = document.createElement("h2");
+  // add content to h2
+  h2.textContent = "Timer";
+  //append to section
+  section.append(h2);
+
+  // create div element
+  const timerDiv = document.createElement("div");
+  // add class attribute
+  timerDiv.setAttribute("class", "timer");
+  //append to section
+  section.append(timerDiv);
+
+  // create p element
+  const paragraph = document.createElement("p");
+  // add the text content
+  paragraph.textContent = "Time remaining: ";
+  //append to div
+  timerDiv.append(paragraph);
+
+  //create span
+  const span = document.createElement("span");
+  //add span attribute
+  span.setAttribute("id", "time-span");
+  // add the text content
+  span.textContent = timerValue;
+  //append to div
+  paragraph.append(span);
 };
 
 const renderQuestionSection = () => {
+  console.log("render question section");
   // use HTML as guide and build in JS
   // append section to main
   // add click event listener on #question-section
@@ -82,11 +134,19 @@ const renderQuizCompleteSection = () => {
 
 const startQuiz = () => {
   // remove start section
+  contentSection.remove();
+
   // start timer
+  startTimer();
+
   // render timer section
+  renderTimerSection();
+
   // render question section
+  renderQuestionSection();
 };
 
 // add event listeners
+startButton.addEventListener("click", startQuiz);
 // add document on load event listener
 // add start button click event listener
